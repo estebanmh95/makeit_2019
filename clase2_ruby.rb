569a62c4f5fdf1c1b3475af2bde089dba6e3ab89
Esteban <estebanmh95@gmail.com>
2019-03-05 18:38:32 -0500

    Ejercicios con if

---
+++ clase2_ruby.rb
@@ -0,0 +1,58 @@
+# print "Ingrese un numero: "
+# a = gets.chomp.to_i
+#
+# if a > 10
+#   puts "El numero es mayor que 10"
+# else
+#   puts "El numero es menor o igual que 10"
+# end
+#----------------------------------------
+
+# a = rand(4)
+#
+# puts "Adivina el numero que estoy pensando: "
+#
+# b = gets.chomp.to_i
+#
+# if a==b
+#   puts "Felicitaciones lo lograste"
+# else
+#   puts "Sigue intentando"
+# end
+#----------------------------------------
+
+# print "Por favor ingrese un numero: "
+#
+# a = gets.chomp.to_i
+#
+# if a % 5 == 0
+#   puts "El numero #{a} es múltiplo de 5"
+# else
+#   puts "El numero #{a} no es múltiplo de 5"
+# end
+#----------------------------------------
+# print "Escribe tu peso en kg: "
+# num1 = gets.chomp
+# print "Escribe cuanto mides en m: "
+# num2 = gets.chomp
+#
+# peso = (num1.to_f)/((num2.to_f)**2)
+#
+# if peso < 18.5
+#
+#   puts "Tu BMI es #{peso.round(2)} y estas bajo de peso"
+#
+# elsif peso >= 18.5 && peso < 25
+#
+#   puts "Tu BMI es #{peso.round(2)} y estas en peso normal"
+#
+# elsif peso >= 25 && peso < 30
+#
+#   puts "Tu BMI es #{peso.round(2)} y estas en sobrepeso"
+#
+# elsif peso >= 30
+#
+#   puts "Tu BMI es #{peso.round(2)} y eres obeso"
+#
+# end
+#----------------------------------------
--- holaMundo.rb
+++ holaMundo.rb
@@ -1,6 +1,51 @@
+#----------------------------------------
+# print "Indique el lado del cuadrado: "
+# Var1able = gets.chomp
+# perim = Var1able.to_i * 4
+# area = (Var1able.to_i) ** 2
+# puts "El perimetro del cuadrado de lado " + Var1able + " es #{perim} y el area del cuadrado de lado " + Var1able + " es #{area}"
+#----------------------------------------
+# print "Escriba lo que quiera: "
+# nombre = gets.chomp
+#
+# puts nombre
+#----------------------------------------
+# print "Escribe tu nombre: "
+# nombre1 = gets.chomp
+#
+# puts ""
+# puts "Hola #{nombre1}!"
+#----------------------------------------
+# print "Escribe un numero: "
+# num1 = gets.chomp
+# print "Escribe otro numero: "
+# num2 = gets.chomp
+# suma  = num1.to_i + num2.to_i
+# puts ""
+# puts "La suma de ambos es #{suma}"
 
-puts "Indique el lado del cuadrado"
-Var1able = gets.chomp
-perim = Var1able.to_i * 4
-area = (Var1able.to_i) ** 2
-puts "El perimetro del cuadrado de lado " + Var1able + " es #{perim} y el area del cuadrado de lado " + Var1able + " es #{area}"
+#----------------------------------------
+# t = Time.now
+# Año = t.strftime("%Y")
+# print "En que año naciste: "
+# num1 = gets.chomp
+# edad = Año.to_i - num1.to_i
+# if edad < 0
+#   puts "Imposible! Aun no haz nacido"
+# elsif edad> 120 && edad > 0
+#   puts "Imposible! Nadie tiene esa edad"
+# elsif edad==1
+#   puts "Tienes 1 año"
+# else
+#   puts "Tienes #{edad} años"
+# end
+#----------------------------------------
+print "Escribe tu peso en kg: "
+num1 = gets.chomp
+print "Escribe cuanto mides en m: "
+num2 = gets.chomp
+
+peso = (num1.to_f)/((num2.to_f)**2)
+
+puts "Tu BMI es #{peso.round(2)}"
+#----------------------------------------
