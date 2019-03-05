040c8882ac763c8214b41e5255f004725e3d5384
Esteban <estebanmh95@gmail.com>
2019-03-04 20:08:08 -0500

    Programa Ruby mejorado

--- holaMundo.rb
+++ holaMundo.rb
@@ -1,6 +1,6 @@
 
 puts "Indique el lado del cuadrado"
 Var1able = gets.chomp
-perim = Var1able.to_i + Var1able.to_i
+perim = Var1able.to_i * 4
 area = (Var1able.to_i) ** 2
-puts "El perimetro del cuadrado es #{perim} y el area es #{area}"
+puts "El perimetro del cuadrado de lado " + Var1able + " es #{perim} y el area del cuadrado de lado " + Var1able + " es #{area}"
