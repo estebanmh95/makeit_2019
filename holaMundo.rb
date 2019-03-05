79250f919426879ad48a1daacc63d476437a91c0
Esteban <estebanmh95@gmail.com>
2019-03-04 20:02:45 -0500

    Programa Ruby

--- holaMundo.rb
+++ holaMundo.rb
@@ -1,4 +1,6 @@
-=begin
-a = "Hola Mundo "
-puts a + "mi edad es #{ 45}"
-=end
+
+puts "Indique el lado del cuadrado"
+Var1able = gets.chomp
+perim = Var1able.to_i + Var1able.to_i
+area = (Var1able.to_i) ** 2
+puts "El perimetro del cuadrado es #{perim} y el area es #{area}"
